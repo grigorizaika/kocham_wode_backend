1e5712c3b992fe31f9e76eca49c5033e011bad89
grgr <grgr@grgr.home>
2020-06-17 13:53:10 +0200

    made root page redirect automatically infer root domain page (without subdomains) if url is a domain name

--- kocham_wode_backend/views.py
+++ kocham_wode_backend/views.py
@@ -1,4 +1,14 @@
-from django.http import HttpResponseRedirect
- 
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import resolve
+
+
+def get_root_url(request):
+    uri_parts = request.build_absolute_uri().split('.')
+    
+    return ('.'.join(uri_parts[-2:]) 
+            if uri_parts[-1].isalpha() 
+            else '')
+
 def start(request):
-    return HttpResponseRedirect('https://kochamwode.pl/')
+    return HttpResponseRedirect(get_root_url(request))
+    
