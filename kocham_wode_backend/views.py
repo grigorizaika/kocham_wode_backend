f614edf275e66e8690acdeab3d7b2c3fab29557a
grgr <grgr@grgr.home>
2020-06-17 13:20:51 +0200

    changed blank root page to redirect

--- kocham_wode_backend/views.py
+++ kocham_wode_backend/views.py
@@ -1,8 +1,4 @@
-from django.http import HttpResponse
- 
+from django.http import HttpResponseRedirect
  
 def start(request):
-    response = HttpResponse()
-    response.write('<h2>Kochamwodę API</h2>')
-    response.write('<a href="https://kochamwode.pl">główna</p>')
-    return response
+    return HttpResponseRedirect('https://kochamwode.pl/')
