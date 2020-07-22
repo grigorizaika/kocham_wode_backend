2e6ee60fc402d8824a61482755e14db688273a29
Grigori Zaika <grgr@localhost.localdomain>
2020-07-22 11:02:26 +0200

    Changed preferences fields to positive integers

--- preferences/models.py
+++ preferences/models.py
@@ -1,17 +1,13 @@
 from decimal import Decimal
 from django.db import models
-from django.core.validators import MinValueValidator
 from users.models import User
 
 
 class Preferences(models.Model):
     age = models.PositiveIntegerField(default=0, blank=True, null=True)
-    weight = models.DecimalField(
-        max_digits=4, decimal_places=1, default=0.01,
-        validators=[MinValueValidator(Decimal('0.01'))],
-        blank=True, null=True)
-    cup_vol = models.IntegerField(default=200)
-    height = models.IntegerField(default=0, blank=True, null=True)
+    weight = models.PositiveIntegerField(default=0)
+    cup_vol = models.PositiveIntegerField(default=200)
+    height = models.PositiveIntegerField(default=0, blank=True, null=True)
     is_male = models.BooleanField(default=False) # NOTE: yk
     user =  models.OneToOneField(
         'users.User',
