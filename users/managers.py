e3989cf86f759e9859444b590ab2d603258f44a4
Grigori Zaika <grgr@localhost.localdomain>
2020-07-27 18:37:53 +0200

    removed surname field from UserManager methods

--- users/managers.py
+++ users/managers.py
@@ -7,14 +7,13 @@ from aws_utils.cognito import create_cognito_user
 class CustomUserManager(BaseUserManager):
     use_in_migrations = True
 
-    def _create_base_user(self, email, name, surname, password=None):
+    def _create_base_user(self, email, name, password=None):
         # try without this
         if not email:
             raise ValueError('An email address is required')
         
         user = self.model(email=self.normalize_email(email),
-                          name=name,
-                          surname=surname)
+                          name=name)
         user.set_password(password)
         user.save()
 
@@ -28,8 +27,8 @@ class CustomUserManager(BaseUserManager):
 
         return cognito_response
 
-    def create_user(self, email, name, surname, password=None):
-        user = self._create_base_user(email, name, surname, password)
+    def create_user(self, email, name, password=None):
+        user = self._create_base_user(email, name, password)
 
         try:
             attributes = {
@@ -43,8 +42,8 @@ class CustomUserManager(BaseUserManager):
 
         return user
 
-    def create_superuser(self, email, name, surname, password=None):
-        user = self._create_base_user(email, name, surname, password)
+    def create_superuser(self, email, name, password=None):
+        user = self._create_base_user(email, name, password)
         
         try:
             attributes = {
@@ -63,17 +62,12 @@ class CustomUserManager(BaseUserManager):
         return user
 
     def get_or_create_for_cognito(self, payload):
-        # TODO: remove print statement
-        print('Payload:', payload)
-
         try:
             return self.get(email=payload['email'])
         except self.model.DoesNotExist:
             user = self._create_base_user(
                 email=payload['email'], 
-                name=payload.get('given_name', None),
-                surname=payload.get('family_name', None))
-
+                name=payload.get('given_name', None))
             is_administrator = (
                 True if payload['custom:is_administrator'].lower() == "true"
                 else False
