58047d9a319d8a883944f31bafd1f19955cdad47
grigorizaika <gregory.zaika@gmail.com>
2020-08-17 17:19:03 +0200

    Changed default values in preferences fields to null

--- preferences/models.py
+++ preferences/models.py
@@ -4,11 +4,11 @@ from users.models import User
 
 
 class Preferences(models.Model):
-    age = models.PositiveIntegerField(default=0, blank=True, null=True)
-    weight = models.PositiveIntegerField(default=0)
+    age = models.PositiveIntegerField(default=None, blank=True, null=True)
+    weight = models.PositiveIntegerField(default=None, blank=True, null=True)
     cup_vol = models.PositiveIntegerField(default=200)
-    height = models.PositiveIntegerField(default=0, blank=True, null=True)
-    is_male = models.BooleanField(default=False) # NOTE: yk
+    height = models.PositiveIntegerField(default=None, blank=True, null=True)
+    is_male = models.BooleanField(default=None, blank=True, null=True)
     user =  models.OneToOneField(
         'users.User',
         on_delete=models.CASCADE)
